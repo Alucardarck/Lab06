449337df05aacf11987fb1082c02a7a181d47240
Martin Leon Preciado <Martin@macbook-air-de-martin.local>
2016-07-17 17:51:09 -0500

    Initial Commit

---
+++ Lab06/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lab06
+//
+//  Created by Martin Leon Preciado on 17/07/16.
+//  Copyright © 2016 UAG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Lab06/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Lab06
+//
+//  Created by Martin Leon Preciado on 17/07/16.
+//  Copyright © 2016 UAG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
